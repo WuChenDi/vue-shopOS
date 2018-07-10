6c6ff5fd65aba9e64a5047e7b917ee31211c8f77
WuChenDi <996194720@qq.com>
2018-07-10 08:42:42 +0800

    路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
+import GoodsList from '@/views/GoodsList'
 
 Vue.use(Router)
 
 export default new Router({
+  // 去除项目url中的#
+  mode: 'history',
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/goods/:goodsId/name/:name',
+      name: 'GoodsList',
+      component: GoodsList
     }
   ]
 })
---
+++ src/views/GoodsList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    这是商品列表页面<br>
+    商品ID为:<span>{{$route.params.goodsId}}</span><br>
+    商品名称为:<span>{{$route.params.name}}</span><br>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "hello vue"
+    };
+  }
+};
+</script>
